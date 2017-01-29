56ca9c6c5efe41b6f0322a40068b8d358bc95d99
Bryan Chen <bryanc.chen@gmail.com>
2017-01-28 20:32:09 -0800

    Clean tests.

--- test.js
+++ test.js
@@ -2,42 +2,36 @@ var expect = require('chai').expect;
 var SortedList = require('./');
 
 describe('sorted-list', function(){
-    var target, target2;
+    var rawdata = [10, 5, 22, 2, -2, 0, 11, -1, 7, 2, 8, -3];
 
     it('validate order of the inserted elements', function(){
-        target = new SortedList();
+        var sorted = new SortedList();
+        rawdata.forEach(function(d){sorted.insert(d);});
 
-        target.insert(10);
-        target.insert(5);
-        target.insert(17);
-        target.insert(2);
-        target.insert(-2);
-        target.insert(0);
-
-        var sorted = target.toArray();
-        expect(sorted).to.eql([-2, 0, 2, 5, 10, 17]);
+        expect(sorted.toArray()).to.eql([-3, -2, -1, 0, 2, 2, 5, 7, 8, 10, 11, 22]);
     });
 
     it('validate multiple instances of sorted list with different comparer', function(){
-        target2 = new SortedList(function(a,b){return a.key-b.key;});
-        target2.insert({key: 10});
-        target2.insert({key: 5});
-        target2.insert({key: 22});
-        target2.insert({key: 2});
-        target2.insert({key: -2});
-        target2.insert({key: 0});
+        var sorted = new SortedList();
+        rawdata.forEach(function(d){sorted.insert(d);});
 
-        target.insert(10);
-        var sorted = target.toArray();
-        expect(sorted).to.eql([-2, 0, 2, 5, 10, 10, 17]);
+        var sorted2 = new SortedList(function(a,b){return a.key-b.key;});
+        sorted2.insert({key: 10});
+        sorted2.insert({key: 5});
+        sorted2.insert({key: 22});
+        sorted2.insert({key: 2});
+        sorted2.insert({key: -2});
+        sorted2.insert({key: 0});
 
-        var sorted2 = target2.toArray().map(function(d){return d.key;});
-        expect(sorted2).to.eql([-2, 0, 2, 5, 10, 22]);
+        var sortedArray2 = sorted2.toArray().map(function(d){return d.key;});
+        expect(sortedArray2).to.eql([-2, 0, 2, 5, 10, 22]);
     });
 
     it('validate length property counts correctly.', function(){
-        expect(target.length).to.equal(7);
-        expect(target2.length).to.equal(6);
+        var sorted = new SortedList();
+        rawdata.forEach(function(d){sorted.insert(d);});
+
+        expect(sorted.length).to.equal(rawdata.length);
     });
 
     var randomSize = 1000;
@@ -57,13 +51,10 @@ describe('sorted-list', function(){
     });
 
     it('validate cut function', function(){
-        var raw = [10, 5, 22, 2, -2, 0, 11, -1, 7, 2, 8, -3];
         var sorted = new SortedList();
-        sorted.insertBatch(raw);
-        sorted.print();
+        sorted.insertBatch(rawdata);
+        //sorted.print();
         
-        expect(sorted.toArray()).to.eql([-3, -2, -1, 0, 2, 2, 5, 7, 8, 10, 11, 22]);
-
         var removed = sorted.cut(9, 'left', true);
         expect(removed).to.eql([-3, -2, -1, 0, 2, 2, 5, 7, 8]);
         expect(sorted.toArray()).to.eql([10, 11, 22]);
