e014dacf6e29bcb1b975e65dcb0fd5a6f7534929
Bryan Chen <bryanc.chen@gmail.com>
2017-01-29 17:57:35 -0800

    Format the output of abtest.

--- abtest.js
+++ abtest.js
@@ -10,18 +10,41 @@ var data = [];
 for(var i=0;i<sampleSize;i++)
     data.push(Math.random());
 
-function perfTest(SortedListClass, data, scenario){
+function perfTest(SortedListClass, data){
     var sorted = new SortedListClass();
 
-    console.time(scenario);
+    var time = process.hrtime();
     sorted.insertBatch(data);
-    console.timeEnd(scenario);
+    var diff = process.hrtime(time);
+
+    return diff[0]*1e9+diff[1];
 }
 
 var current = require('./index.js');
 var base = require('./.abtest/index.js');
 
-for(var i=0;i<3;i++){
-    perfTest(current, data, i+ ": Insertion - Current");
-    perfTest(base, data, i+ ": Insertion - Base");
+function perfDelta(baseET, newET){
+    var r = (baseET/newET) - 1;
+    if(r>0)
+        return (r*100).toString().slice(0, 5) + '% Better';
+    else if(r<0)
+        return (-r*100).toString().slice(0, 5) + '% Worse';
+    else
+        return 'The same.';
 }
+
+console.log(' \t \t\t Base \t\t Current \t\t Delta%');
+var measureCount = 5;
+var avgBasePerf = 0, avgCurrentPerf = 0;
+for(var i=0;i<measureCount;i++){
+    var basePerf = perfTest(base, data);
+    avgBasePerf+=basePerf;
+    var currentPerf = perfTest(current, data);
+    avgCurrentPerf += currentPerf;
+
+    console.log(' \t '+ i + '\t '+basePerf + 'ns\t\t'+ currentPerf +'ns\t\t'+perfDelta(basePerf, currentPerf));
+}
+avgBasePerf/=measureCount;
+avgCurrentPerf/=measureCount;
+console.log(' Average\t '+avgBasePerf + 'ns\t\t'+ avgCurrentPerf +'ns\t\t'+perfDelta(avgBasePerf, avgCurrentPerf));
+
